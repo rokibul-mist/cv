524bba7ae0b7fe4cc2bd611c8cc851e2ee812fe9
rokibulhasan-munna <rokibulhasan9102@gmail.com>
2024-11-07 00:39:08 +0600

    update

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/* Function to open the sidebar */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  /* Function to close the sidebar */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+  
